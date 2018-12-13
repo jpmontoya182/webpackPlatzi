e58631a5d4313c8e00d00a38140a6dc02956641c
Juan Pablo Montoya Cardona <jpmontoya182@gmail.com>
2018-12-13 11:42:52 -0500

    it was cleaned folders and rename them

---
+++ 16.prevent-duplications/webpack.config.js
@@ -0,0 +1,109 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+    entry: {
+        home: path.resolve(__dirname, 'src/js/index.js'),
+        contact : path.resolve(__dirname, 'src/js/contact.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    { 
+                        loader: 'css-loader', 
+                        options: {
+                            modules: true, 
+                            importLoaders : 1
+                        }
+                    },
+                    { loader: 'postcss-loader'},                    
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' },
+                ]
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {loader : 'css-loader'}, 
+                    { 
+                        loader: 'stylus-loader', 
+                        options : {
+                            use : [
+                                require('nib'),
+                                require('rupture')
+                            ], 
+                            import : [
+                                '~nib/lib/nib/index.styl',
+                                '~rupture/rupture/index.styl'
+                            ]
+                        }
+                    },  
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader' },
+                    { 
+                        loader: 'less-loader', 
+                        options: {
+                            noIeCompat: true
+                        } 
+                },
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                }
+            },
+            {
+                test: /\.(jpg|png|gif|woff|eot|ttf|svg)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 100000,
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',            
+        }), 
+    ],
+    resolveLoader: {
+        moduleExtensions: ['-loader'],
+    },
+    optimization: {
+        splitChunks: {
+          cacheGroups: {
+            commons: {
+              name: 'commons',
+              chunks: 'initial', 
+              minChunks: 2
+            }
+          }
+        }
+    }
+
+}
